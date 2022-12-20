6d20df9093559c661645a1b2d2dc77ac5568af40
qufdhks <ghlehd57@naver.com>
2022-12-20 17:30:53 +0900

    레이대화

---
+++ Assets/HDong/Scripts/ObjData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjData : MonoBehaviour
+{
+    //[SerializeField] private int id;
+    //[SerializeField] private bool isNpc;
+    public int id;
+    public bool isNpc;
+
+    //public int Getid { get { return id; } }
+    //public bool GetisNpc { get { return isNpc; } }
+}
