53c38516eb848e37338df01c8c2658480e5e79aa
KIMTAEYEON99 <115608446+KIMTAEYEON99@users.noreply.github.com>
2022-12-20 12:39:25 +0900

    ㅇㄹ

---
+++ Assets/02_Scripts/Crafting.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crafting : MonoBehaviour
+{
+
+    public enum CraftableItem { AXE};
+    public enum ResourceItem { WOOD, STONE };
+
+    public int wood;
+    public int stone;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
