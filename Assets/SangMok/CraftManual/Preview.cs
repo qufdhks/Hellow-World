18b8d7f17a278a52ac1ccabf59388ab79cb854b8
sangmok222 <116792369+sangmok222@users.noreply.github.com>
2022-12-23 16:09:18 +0900

    건축완료

---
+++ Assets/SangMok/CraftManual/Preview.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Preview : MonoBehaviour
+{
+    Transform tf_Player = null;
+
+    private void Awake()
+    {
+        tf_Player = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        transform.position = tf_Player.position + (tf_Player.forward * 10f) + new Vector3(0f, -1.2f, 0f);
+        transform.LookAt(tf_Player.position);
+        
+    }
+
+}
---
+++ Assets/SangMok/CraftManual/PreviewObject.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviewObject : MonoBehaviour
+{
+    private List<Collider> colliderList = new List<Collider>(); //충돌 콜라이더
+
+
+    [SerializeField]
+    private int layerGround;//지상
+    private const int IGNORE_RAYCAST_LAYER = 2;
+
+    [SerializeField]
+    private Material green;
+    [SerializeField]
+    private Material red;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        ChangeColor();
+    }
+
+    private void ChangeColor()
+    {
+        if (colliderList.Count > 0)
+            SetColor(red);
+        else
+            SetColor(green);
+    }
+
+    private void SetColor(Material mat)
+    {
+        foreach(Transform tf_Child in this.transform)
+        {
+            var newMaterials = new Material[tf_Child.GetComponent< Renderer > ().materials.Length];
+
+            for(int i = 0; i < newMaterials.Length; i++)
+            {
+                newMaterials[i] = mat;
+            }
+            tf_Child.GetComponent<Renderer>().materials = newMaterials;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer != layerGround && other.gameObject.layer != IGNORE_RAYCAST_LAYER)
+            colliderList.Add(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer != layerGround && other.gameObject.layer != IGNORE_RAYCAST_LAYER)
+            colliderList.Remove(other);
+    }
+
+    public bool IsBuildable()
+    {
+        return colliderList.Count == 0;  
+    }
+}
