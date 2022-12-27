827416c4b6738a6b96347676a076eeef1a702443
sangmok222 <116792369+sangmok222@users.noreply.github.com>
2022-12-27 17:58:57 +0900

    낚시22

--- Assets/SangMok/Fishing/fishing.cs
+++ Assets/SangMok/Fishing/fishing.cs
@@ -9,17 +9,20 @@ public class fishing : MonoBehaviour
     public float F_Speed = 1f;
 
     public Transform m_Target;
+    
     public float m_InitialAngle = 70f; // 처음 날라가는 각도
     private Rigidbody m_Rigidbody;
 
-    private float interval = 1f;
+    private float interval = 0.3f;
 
     private bool isAction = false;
+
+
 
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
-        
+       
     }
 
     void Update()
@@ -30,7 +33,7 @@ public class fishing : MonoBehaviour
             m_Rigidbody.velocity = velocity;
         }
 
-        StartCoroutine(FishingCoroutine());
+        
     }
 
     public Vector3 GetVelocity(Vector3 player, Vector3 target, float initialAngle)
@@ -58,6 +61,32 @@ public class fishing : MonoBehaviour
         return finalVelocity;
     }
 
+    //public Vector3 ComVelocity(Vector3 target, Vector3 player, float initialAngle)
+    //{
+    //    float gravity = Physics.gravity.magnitude;
+    //    float angle = initialAngle * Mathf.Deg2Rad;
+
+    //    Vector3 planarTarget = new Vector3(target.x, 0, target.z);
+    //    Vector3 planarPosition = new Vector3(player.x, 0, player.z);
+
+    //    float distance = Vector3.Distance(planarTarget, planarPosition);
+    //    float yOffset = player.y - target.y;
+
+    //    float initialVelocity
+    //        = (1 / Mathf.Cos(angle)) * Mathf.Sqrt((0.5f * gravity * Mathf.Pow(distance, 2)) / (distance * Mathf.Tan(angle) + yOffset));
+
+    //    Vector3 velocity
+    //        = new Vector3(0f, initialVelocity * Mathf.Sin(angle), initialVelocity * Mathf.Cos(angle));
+
+    //    float angleBetweenObjects
+    //        = Vector3.Angle(Vector3.forward, planarTarget - planarPosition) * (target.x > player.x ? 1 : -1);
+    //    Vector3 finalVelocity
+    //        = Quaternion.AngleAxis(angleBetweenObjects, Vector3.up) * velocity;
+
+    //    return finalVelocity;
+    //}
+
+
 
     private void OnCollisionEnter(Collision col)
     {
@@ -68,22 +97,34 @@ public class fishing : MonoBehaviour
 
             isAction = true;
             StartCoroutine(MoveFish());
+            StartCoroutine(FishingCoroutine());
         }
     }
 
      private IEnumerator FishingCoroutine()
      {
-        if(gameObject.name == "Water")
-        { 
-            //물고기 잡기
-        }
-        yield return new WaitForSeconds(interval);
+        while (isAction)
+        {
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                //물고기 잡기
+                //fiShing.transform.SetParent(handTr);//handTr 찌
+                //fiShing.transform.localPosition = Vector3.zero;
+                Debug.Log("잡음");
+                isAction = false;
+                //Vector3 velocity = ComVelocity(transform.position, m_Target.position, m_InitialAngle);
+                //m_Rigidbody.velocity = velocity;
+            }
 
-        //isAction = false;
+            yield return new WaitForSeconds(interval);
+            //yield return null;
+
+            //isAction = false;
+        }
      }
 
      private IEnumerator MoveFish()
-    {
+     {
         Vector3 curPos = transform.position;
         while(isAction)
         {
@@ -92,5 +133,5 @@ public class fishing : MonoBehaviour
             yield return null;
         }
 
-    }
+     }
 }
