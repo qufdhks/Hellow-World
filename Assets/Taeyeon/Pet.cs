107ff018915b4cb8627d10350053d5a81799f64f
KIMTAEYEON99 <115608446+KIMTAEYEON99@users.noreply.github.com>
2023-01-03 12:46:03 +0900

    친화력

--- Assets/Taeyeon/Pet.cs
+++ Assets/Taeyeon/Pet.cs
@@ -7,22 +7,33 @@ public class Pet : MonoBehaviour
 {
     NavMeshAgent nav;
     GameObject target;
-
+    ObjData data;
+   
+    [SerializeField]  private int liking;
     public void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+        data = GetComponent<ObjData>();
         target = GameObject.Find("petPosition");
     }
 
     private void Update()
     {
-        if(nav.destination != target.transform.position)
+        if (liking >= data.needliking)
         {
-            nav.SetDestination(target.transform.position);
+            if (nav.destination != target.transform.position)
+                nav.SetDestination(target.transform.position);
         }
         else
         {
             nav.SetDestination(transform.position);
         }
     }
+
+    public void PlusLiking()
+    {
+        liking += 10;
+    }
+
+
 }
