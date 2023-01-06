251b00a73831cf4ad7c1cee428cc4865bdda9c06
KIMTAEYEON99 <115608446+KIMTAEYEON99@users.noreply.github.com>
2023-01-06 16:20:27 +0900

    퀘스트 조금추가 맵 조금 수정 ^^

--- Assets/HDong/Scripts/QuestManager.cs
+++ Assets/HDong/Scripts/QuestManager.cs
@@ -20,10 +20,10 @@ public class QuestManager : MonoBehaviour
 
     void GenerateData()
     {
-        questList.Add(10, new QuestData("의문의 마을", new int[] {1000, 2000, 4000}));
+        questList.Add(10, new QuestData("의문의 마을", new int[] {1000, 2000}));
         questList.Add(20, new QuestData("마을 적응하기(1)", new int[] {1000, 2000}));
-        questList.Add(20, new QuestData("마을 적응하기(2)", new int[] {4000}));
-        questList.Add(30, new QuestData("퀘스트 올 클리어" , new int[] {0}));
+        questList.Add(30, new QuestData("마을 적응하기(2)", new int[] {4000}));
+        questList.Add(40, new QuestData("퀘스트 올 클리어" , new int[] {0}));
     }
 
     public int GetQuestTalkIndex(int id)
