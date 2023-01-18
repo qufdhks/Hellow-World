ca65e28549eb407bf7ce1916a6de77c0ca08dccb
T0m4t00 <kzwizz@yahoo.com>
2023-01-18 09:47:39 +0900

    Swing

--- Assets/Taeyeon/Scripts/Axe.cs
+++ Assets/Taeyeon/Scripts/Axe.cs
@@ -15,29 +15,29 @@ public class Axe : MonoBehaviour
 
         Vector3 dir = new Vector3(0, 0, h);
 
-        transform.Translate(dir * 5f * Time.deltaTime);
+        //transform.Translate(dir * 5f * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Tree"))
         {
-            
+
             //GetVelocity(wood.transform.position, pos, 70f);
 
             count++;
-            Instantiate(woodPfs, transform.position + new Vector3(0,0,count/2), Quaternion.identity);
+            Instantiate(woodPfs, transform.position + new Vector3(0, 0, count / 2), Quaternion.identity);
             if (count >= 3)
             {
                 Instantiate(stump, other.transform.position, Quaternion.identity);
-                count = 0;// ī��Ʈ �ʱ�ȭ
+                count = 0;// ī��Ʈ �ʱ�ȭ
                 Destroy(other.gameObject);
             }
         }
 
     }
 
-    
 
-    
+
+
 }
