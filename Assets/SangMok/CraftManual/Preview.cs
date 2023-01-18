e420fa73f914d30bde7fbc6dab1d8e7ef75810b7
sangmok222 <116792369+sangmok222@users.noreply.github.com>
2023-01-18 15:50:44 +0900

    맵 변경

--- Assets/SangMok/CraftManual/Preview.cs
+++ Assets/SangMok/CraftManual/Preview.cs
@@ -13,9 +13,10 @@ public class Preview : MonoBehaviour
 
     private void Update()
     {
-        transform.position = tf_Player.position + (tf_Player.forward * 10f) + new Vector3(0f, -1.5f, 0f);
+
+        transform.position = tf_Player.position + (tf_Player.forward * 5f) + new Vector3(0f, 0f, 0f);
         transform.LookAt(tf_Player.position);
-        
+
     }
 
 }
