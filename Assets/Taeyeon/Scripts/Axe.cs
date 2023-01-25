a8dff6194d0b412611fa5300a4bbb99c1e7d6d18
qufdhks <ghlehd57@naver.com>
2023-01-25 12:05:12 +0900

    도끼 수정

--- Assets/Taeyeon/Scripts/Axe.cs
+++ Assets/Taeyeon/Scripts/Axe.cs
@@ -16,6 +16,7 @@ public class Axe : MonoBehaviour
     private void Start()
     {
         collider = GetComponent<BoxCollider>();
+        collider.enabled = false;
     }
 
     private void Update()
@@ -30,7 +31,7 @@ public class Axe : MonoBehaviour
         {
 
             //GetVelocity(wood.transform.position, pos, 70f);
-
+            collider.enabled = false;
             count++;
             Instantiate(woodPfs, transform.position + new Vector3(0, 0, count / 2), Quaternion.identity);
             if (count >= 3)
@@ -45,6 +46,7 @@ public class Axe : MonoBehaviour
 
     IEnumerator OnOff()
     {
+        yield return new WaitForSeconds(0.5f);
         collider.enabled = true;
         yield return new WaitForSeconds(0.5f);
         collider.enabled = false;
