78a17dcf28be2a7499d83f3f5db00175a3e35bd1
qufdhks <ghlehd57@naver.com>
2023-01-25 10:54:51 +0900

    오프닝 수정

--- Assets/02_Scripts/OpeningManager.cs
+++ Assets/02_Scripts/OpeningManager.cs
@@ -6,13 +6,18 @@ using UnityEngine.UI;
 public class OpeningManager : MonoBehaviour
 {
     [SerializeField] private GameObject anyText;
-    [SerializeField] private GameObject menu;
+    [SerializeField] private Button[] buttons;
+    [SerializeField] private Image menu;
+    [SerializeField] private Text[] texts;
+    float fadeTime = 1.5f;
 
     private void Update()
     {
         if (Input.anyKeyDown && anyText.activeSelf)
         {
-            menu.SetActive(true);
+            StartCoroutine(FadeColor());
+            foreach(Button button in buttons)
+                button.enabled = true;
             anyText.SetActive(false);
         }
     }
@@ -28,5 +33,29 @@ public class OpeningManager : MonoBehaviour
         _text.text = "<color=black>" + _text.text + "</color>";
         yield return new WaitForSeconds(0.2f);
         _text.text = text;
+    }
+
+    IEnumerator FadeColor()
+    {
+        float curruntTime = 0;
+        float percent = 0;
+        
+        while(percent < 1)
+        {
+            curruntTime += 0.003f;
+            percent = curruntTime / fadeTime;
+
+            Color menuColor = menu.color;
+            foreach(Text text in texts)
+            {
+                Color textColor = text.color;
+                textColor.a = Mathf.Lerp(0, 1, percent);
+                text.color = textColor;
+            }
+            menuColor.a = Mathf.Lerp(0, 1, percent);
+            menu.color = menuColor;
+
+            yield return null;
+        }
     }
 }
